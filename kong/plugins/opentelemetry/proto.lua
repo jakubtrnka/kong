1b6545f3cfcd726995f7101b6175c4a32475eb10
Pavol Ostertag <pavol.ostertag@eset.sk>
2023-01-02 12:00:57 +0100

    plugins/grpc-gateway: add full support of PROTO - JSON conversions
    fix: error messages now comply https://github.com/golang/go/wiki/CodeReviewComments#error-strings

--- kong/plugins/opentelemetry/proto.lua
+++ kong/plugins/opentelemetry/proto.lua
@@ -1,14 +1,14 @@
-local grpc = require "kong.tools.grpc"
+local grpc_tools = require "kong.tools.grpc"
 
 local proto_fpath = "opentelemetry/proto/collector/trace/v1/trace_service.proto"
 local proto_logs_fpath = "opentelemetry/proto/collector/logs/v1/logs_service.proto"
 
 local function load_proto()
-  local grpc_util = grpc.new()
-  local protoc_instance = grpc_util.protoc_instance
+  local grpc = grpc_tools.new()
+  local protoc = grpc.protoc
 
-  protoc_instance:loadfile(proto_fpath)
-  protoc_instance:loadfile(proto_logs_fpath)
+  protoc:loadfile(proto_fpath)
+  protoc:loadfile(proto_logs_fpath)
 end
 
 load_proto()
